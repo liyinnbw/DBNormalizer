33734ad16febee8b3b4082d666e23aa49edc3f74
Linus Tan <linustws00@gmail.com>
2023-04-23 14:46:00 +0800

    allow user input

--- main.py
+++ main.py
@@ -17,15 +17,40 @@ import decomposition
 ###########################################
 
 # Modify from this line onwards to suit your needs
-R = ['A', 'B', 'C']
-FD = [[['A', 'B'], ['C']], [['A'], ['B']], [['B'], ['A']]]
-normalform = normalforms.threenf
+# R = ['A', 'B', 'C', 'D']
+# FD = [[['B', 'C'], ['D']], [['A'], ['B']]]
+# normalform = normalforms.bcnf
+R = input("Enter the list of unique variables (R) separated by commas: ").split(",")
+FD = []
+while True:
+    dep = input("Enter a functional dependency e.g. a,b->c (or enter nothing to stop): ")
+    if not dep:
+        break
+    try:
+        lhs, rhs = dep.split("->")
+        FD.append([lhs.split(","), rhs.split(",")])
+    except ValueError:
+        print("Invalid functional dependency!")
+while True:
+    normalform_name = input("Enter the normal form you want to achieve (2nf, 3nf, or bcnf): ")
+    if normalform_name == "2nf":
+        normalform = normalforms.twonf
+        break
+    elif normalform_name == "3nf":
+        normalform = normalforms.threenf
+        break
+    elif normalform_name == "bcnf":
+        normalform = normalforms.bcnf
+        break
+    else:
+        print("Invalid normal form!")
+
 # Do not modify anything from this line onwards
 
 ###########################################
 # find all candidate keys
 ###########################################
-print('all candiate keys:')
+print('all candidate keys:')
 print(api.candidate_keys(R, FD))
 
 ###########################################
@@ -34,20 +59,19 @@ print(api.candidate_keys(R, FD))
 print('')
 print('')
 print('a minimal cover:')
-minCover = api.min_cover(R,FD)
+minCover = api.min_cover(R, FD)
 for F in minCover:
-    print(F[0],'->',F[1])
+    print(F[0], '->', F[1])
 
 ###########################################
 # check all F in FD for violation of given normal form
 ###########################################
 print('')
-print('')  
-print('check normal form:',normalform.__name__)
-FDsimplified = api.min_cover_step1(R,FD)
+print('')
+print('check normal form:', normalform.__name__)
+FDsimplified = api.min_cover_step1(R, FD)
 for F in FDsimplified:
-    print(F[0],'->',F[1],normalform(R,FDsimplified,F))
-
+    print(F[0], '->', F[1], normalform(R, FDsimplified, F))
 
 ###########################################
 # decomposition to given normal form
@@ -56,12 +80,12 @@ for F in FDsimplified:
 # also verify result against given normal form
 ###########################################
 useSigmaPlus = True
-printStep = False # if true will print steps invovled in deriving the answer
+printStep = False  # if true will print steps invovled in deriving the answer
 print('')
 print('')
-print('decomposition:',normalform.__name__, 'showStep' if printStep else '')
-fragments = decomposition.decompose(R,FD,normalform,useSigmaPlus,printStep)
-decomposition.print_fragments(fragments,normalform)
+print('decomposition:', normalform.__name__, 'showStep' if printStep else '')
+fragments = decomposition.decompose(R, FD, normalform, useSigmaPlus, printStep)
+decomposition.print_fragments(fragments, normalform)
 
 ###########################################
 # synthesize to 3NF (not any form)
@@ -69,11 +93,9 @@ decomposition.print_fragments(fragments,normalform)
 # if not show which dependency is lost
 # also verify result against 3NF
 ###########################################
-printStep = False # if true will print steps invovled in deriving the answer
+printStep = False  # if true will print steps invovled in deriving the answer
 print('')
 print('')
 print('synthesis: threenf', 'showStep' if printStep else '')
-fragments = decomposition.simple_synthesis(R,FD,printStep)
-decomposition.print_fragments(fragments,normalform)
-
-
+fragments = decomposition.simple_synthesis(R, FD, printStep)
+decomposition.print_fragments(fragments, normalform)
